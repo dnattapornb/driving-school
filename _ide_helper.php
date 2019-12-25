e966e5452c0c53e2602741de94802d207a945e9d
Nattaporn Detdee <nattaporn.d@hoteliers.guru>
2019-12-25 16:07:13 +0700

    add, Laravel Roles https://github.com/jeremykenedy/laravel-roles#environment-file
    > composer require jeremykenedy/laravel-roles
    > php artisan vendor:publish --tag=laravelroles
    > php artisan migrate
    > composer dump-autoload
    > php artisan db:seed

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.36 on 2019-12-24 17:02:05.
+ * Generated for Laravel 5.8.36 on 2019-12-25 16:00:52.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
