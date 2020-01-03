7849177cd61e28eb48c10687fc4416cb076ac346
Nattaporn Detdee <nattaporn.d@hoteliers.guru>
2020-01-03 15:25:53 +0700

    configs, barryvdh/laravel-ide-helper

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.8.36 on 2020-01-02 11:38:21.
+ * Generated for Laravel 5.8.36 on 2020-01-03 15:24:22.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
